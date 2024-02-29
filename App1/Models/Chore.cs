4f8fbb5d22896d0202a4707fca5e2fb620da1f55
preki <preki@LAPTOP-D52GU6M7>
2024-02-28 16:14:19 -0800

    Add project files.

---
+++ App1/Models/Chore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+	public class Chore
+	{
+		public string title;
+		public string notes;
+		public Chore(string title, string notes)
+		{
+			this.title = title;
+			this.notes = notes;
+		}
+	}
+	public class ChoreDisplay
+	{
+		Chore chore;
+		bool isExpanded;
+		public bool IsExpanded
+		{
+			get => isExpanded;
+			set => isExpanded = value;
+		}
+		public ChoreDisplay(string title, string notes)
+		{
+			chore = new Chore(title, notes);
+		}
+		public ChoreDisplay(string title) : this(title, "") { }
+		public string Name
+		{
+			get
+			{
+				return chore.title;
+			}
+			set
+			{
+				chore.title = value;
+			}
+		}
+		public string Notes
+		{
+			get
+			{
+				return chore.notes;
+			}
+		}
+	}
+}
---
+++ App1/ViewModels/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,89 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using App1.Models;
+using System.Runtime.InteropServices;
+using Xamarin.Forms;
+using System.Text.RegularExpressions;
+
+namespace App1.ViewModels
+{
+	public class CoffeeEquipmentViewModel : BaseViewModel
+	{
+		private static readonly Regex sWhitespace = new Regex(@"\s+");
+		string newChoreText = "";
+		public string NewChoreTextProperty { get { return newChoreText; } set { newChoreText = value; } }
+		public ObservableRangeCollection<ChoreDisplay> Chores1 { get; }
+		public AsyncCommand RefreshCommand { get; }
+		ChoreDisplay selectedChore;
+		ChoreDisplay previouslySelectedChore;
+		public ChoreDisplay SelectedChore { get => selectedChore; set
+			{
+				if (value != null)
+				{
+					previouslySelectedChore = (ChoreDisplay)value;
+					Application.Current.MainPage.DisplayAlert("aoeu", previouslySelectedChore.Name,"ok");
+					value = null;
+				}
+				selectedChore = value;
+				OnPropertyChanged();
+
+			}
+		}
+		public CoffeeEquipmentViewModel()
+		{
+
+			Chores1 = new ObservableRangeCollection<ChoreDisplay>
+			{
+				new ChoreDisplay("thing1", "Noteaoaoentuh"),
+				new ChoreDisplay("thnig52", "note 222"),
+				new ChoreDisplay("thing32", "note1"),
+				new ChoreDisplay("thing", "thing")
+			};
+
+			RefreshCommand = new AsyncCommand(Refresh);
+			addNewChoreCommand = new MvvmHelpers.Commands.Command(AddChore);
+		}
+		async Task Refresh()
+		{
+			IsBusy = true;
+			await Task.Delay(1000);
+			IsBusy = false;
+		}
+
+		public ICommand addNewChoreCommand { get; }
+		public void AddChore()
+		{
+			if (!IsEmptyText(newChoreText))
+			{
+				var chore = new ChoreDisplay(newChoreText);
+				Chores1.Add(chore);
+				Application.Current.MainPage.DisplayAlert("adding chore", chore.Name, "ok");
+			}
+			else
+			{
+				Application.Current.MainPage.DisplayAlert("text cannot be empty", "Poop", "ok");
+
+			}
+		}
+		public bool IsEmptyText(string text)
+		{
+			foreach(char c in text)
+			{
+				if (!(c == '\n' || c == '\t' || c == ' '))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+}
